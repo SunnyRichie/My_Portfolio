da2dd36e5f8d18f5487694215610f871546285be
SunnyRichie <sunkanmiadeduwon@gmail.com>
2024-11-04 22:33:42 +0100

    updated navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './navbar.css';
 // import logo from '../../assets/updated_logo.svg';
 // import underline from '../../assets/nav_underline.svg';
-import underline from '../../assets/theme_pattern.PNG';
+import underline from '../../assets/theme_pattern.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import menu_open from '../../assets/menu_open.svg';
 import menu_close from '../../assets/menu_close.svg';
